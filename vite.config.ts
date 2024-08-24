87ca3ee669b5205908ecd22b14d8b3660f6224df
Tanja Zakurday <zakurdaytanja@gmail.com>
2024-08-24 14:51:10 +0000

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quiz-app/',
   server: {
     watch: {
       usePolling: true,
-    },}
+    },
+  }
 })
